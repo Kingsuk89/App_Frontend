8c88cd5afdd2a7e5514b2f4452474d261f46d229
kingsuk <kingsuk055@gmail.com>
2024-04-21 13:34:52 +0530

    fix redirect

--- src/router.jsx
+++ src/router.jsx
@@ -1,68 +1,64 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
-import Layout from "./Layout";
-import Home from "./components/home/Home";
-import Course from "./components/course/Course";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
-import Verify from "./components/auth/Verify";
-import RestForm from "./components/auth/RestForm";
-import ResetPass from "./components/auth/ResetPass";
-import Profile from "./components/user/Profile";
-import Contact from "./components/contact/Contact";
-import Protected from "./components/utils/Protected";
+import Layout from './Layout';
+import Home from './components/home/Home';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
+import Verify from './components/auth/Verify';
+import RestForm from './components/auth/RestForm';
+import ResetPass from './components/auth/ResetPass';
+import Profile from './components/user/Profile';
+import Contact from './components/contact/Contact';
+import Protected from './components/utils/Protected';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Protected>
-        <Layout />
-      </Protected>
-    ),
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "course",
-        element: (
-          <div className=" min-h-screen">
-            <Course />
-          </div>
-        ),
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signUp",
-    element: <Signup />,
-  },
-  {
-    path: "/verify/:email",
-    element: <Verify />,
-  },
-  {
-    path: "/reset-form",
-    element: <RestForm />,
-  },
-  {
-    path: "/reset/:email",
-    element: <ResetPass />,
-  },
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <Home />,
+			},
+			{
+				path: '/profile',
+				element: (
+					<Protected>
+						<Profile />
+					</Protected>
+				),
+			},
+			{
+				path: '/contact',
+
+				element: (
+					<Protected>
+						<Contact />
+					</Protected>
+				),
+			},
+		],
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/signUp',
+		element: <Signup />,
+	},
+	{
+		path: '/verify/:email',
+		element: <Verify />,
+	},
+	{
+		path: '/reset-form',
+		element: <RestForm />,
+	},
+	{
+		path: '/reset/:email',
+		element: <ResetPass />,
+	},
 ]);
 
 export default router;
